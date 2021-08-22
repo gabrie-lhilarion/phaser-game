cc2d3468554039f54a8d4a02fde1f7f8aadd55cc
Gabriel Hilarion <gabrielhilarion@gmail.com>
2021-08-22 18:46:41 +0100

    Update welcome.js

--- src/scenes/welcome.js
+++ src/scenes/welcome.js
@@ -10,28 +10,7 @@ class Welcome extends Scene {
   }
 
   preload() {
-    const html = `<html>
-    <head>
-        <style>
-            #input-form {
-                padding: 15px;
-                background-color: #1c9330;
-                border-radius: 4px;
-            }
-            #input-form input {
-                padding: 10px;
-                font-size: 20px;
-                width: 400px;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="input-form">
-            <input type="text" name="name" placeholder="Full Name" />
-        </div>
-    </body>
-</html>`
-    this.load.html('form', html);
+    this.load.html('form', 'assets/index.html');
   }
 
   create() {
